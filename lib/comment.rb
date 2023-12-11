236e7bbe68887958048bc463621ea7dcaf501137
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2023-12-10 23:39:12 -0700

    Add comment parsing and object creation

---
+++ lib/comment.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class Comment
+  attr_reader :task_phid, :author_phid, :phid, :date_created, :date_modified, :content
+
+  def initialize(comment_data)
+    @phid = comment_data['phid']
+    @task_phid = comment_data['objectPHID']
+    @author_phid = comment_data['authorPHID']
+    @date_created = comment_data['dateCreated']
+    @date_modified = comment_data['dateModified']
+    @content = get_content(comment_data['comments'])
+  end
+
+  def get_content(comments)
+    valid_comments = comments.select do |comment|
+      comment['removed'] == false
+    end
+
+    if valid_comments.empty?
+      'NO COMMENT DATA'
+    else
+      valid_comments.first['content']['raw']
+    end
+  end
+end
