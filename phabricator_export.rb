8fb2aab8cc30539c80c606384869743016aba5dd
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2023-12-17 19:10:52 -0700

    Update exporter remaining TODO's comments and command outputs

--- phabricator_export.rb
+++ phabricator_export.rb
@@ -24,34 +24,22 @@ PHAB_HOST = gets.chomp
 # puts 'Please enter your desired Tags for your Discourse topics as a comma seperated list'
 # TAGS = gets.chomp
 
+puts 'Parsing Phabricator issues...'
 issues = PhabricatorParser.new(PHAB_KEY).parse_issues
 
+puts 'Parsing Phabricator users...'
 users = PhabricatorParser.new(PHAB_KEY).parse_users
 
+puts 'Parsing Phabricator comments...'
 comments = PhabricatorParser.new(PHAB_KEY).parse_transactions
 
+puts 'Formatting Phabricator data...'
 formatter = Formatter.new(issues, comments, users)
 formatter.format_data
-formatter.write_file('./discourse_migrate.json')
-# TODO: create a large json payload of relevant data to post to discourse
-# write it to a text file with all the data
-# something like this
-# [{issue_name: issue.name,
-#   issue_id: issue.id,
-#   issue_phid: issue.phid,
-#   issue_title: issue.title,
-#   issue_description: issue.description,
-#   issue_user: {
-#     username: user.username,
-#     user_id: user.id,
-#     user_phid: user.phid,
-#   }
-#   transactions: [{
-#     puts the comments and transaction data here, linked to the user
-#   },
-#   {etc etc }]
-#   }]
-#
-#   then have the DiscoursePoster read the file
-#   make an API call to discourse to create a topic and tags for issues
-#   make sure to slim this file down to only a few issues for testing...nuke them, then do it live
+
+puts 'Writing migration data as JSON...'
+formatter.write_file('./discourse_migration_data.json')
+
+# TODO: have the DiscoursePoster read the file
+# make an API call to discourse to create a topic and tags for issues
+# make sure to slim this file down to only a few issues for testing...nuke them, then do it live
